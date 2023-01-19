31870aeec06fd337f90e53c2cf25a5d392059827
Chirantan Banik <banikchirantan16@gmail.com>
2023-01-20 01:52:54 +0530

    Changes made

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from getpass import getpass
 
-username = "cdgdvygwf"
-password = "dcwgydcywe"
+username = input("Enter in your Username: ")
+password = getpass("Enter your password: ")
 
-url = "https://customer.i-on.in:9443/"
 
-driver = webdriver.Chrome()
-driver.get(url)
+driver = webdriver.Chrome("C:\Drivers\WebDriver\\chromedriver.exe")
+driver.get("https://ecm.smtech.in/ecm/")
+searchbox = driver.find_element_by_xpath('//*[@id="TxtUserName"]').send_keys(username)
+searchpassword = driver.find_element_by_xpath('//*[@id="TxtPassword"]').send_keys(password)
+searchbutton = driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
+searchbox.submit()
+
 
