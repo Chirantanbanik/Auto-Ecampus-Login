a65f82f6c43b96d8f694ad59b75e6413e46271b3
Chirantan Banik <banikchirantan16@gmail.com>
2023-01-21 17:01:26 +0530

    changes made

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from getpass import getpass
+import time
+from selenium.webdriver.common.by import By
 
 username = input("Enter in your Username: ")
 password = getpass("Enter your password: ")
@@ -7,9 +9,12 @@ password = getpass("Enter your password: ")
 
 driver = webdriver.Chrome("C:\Drivers\WebDriver\\chromedriver.exe")
 driver.get("https://ecm.smtech.in/ecm/")
-searchbox = driver.find_element_by_xpath('//*[@id="TxtUserName"]').send_keys(username)
-searchpassword = driver.find_element_by_xpath('//*[@id="TxtPassword"]').send_keys(password)
-searchbutton = driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
-searchbox.submit()
+time.sleep(5)
+searchbox = driver.find_element(by=By.XPATH,value='//*[@id="TxtUserName"]').send_keys(username) 
+searchpassword = driver.find_element(by=By.XPATH,value='//*[@id="TxtPassword"]').send_keys(password)
+searchbutton = driver.find_element(by=By.XPATH,value='//*[@id="btnLogin"]').click()
+print("Sucessfully loggedin!")
+
+
 
 
